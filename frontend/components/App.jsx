a7f55e7f7c412142c993ccbc6b58e12dcd4aea1c
kara <karaliuu@gmail.com>
2020-04-07 14:58:39 -0400

    trying to style home page

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 import SignUpFormContainer from '../components/session_form/signup_form_container';
 import SignInFormContainer from './session_form/signin_form_container';
 import NavBarContainer from './nav_bar/nav_bar_container';
+import Home from './home/home';
 import { AuthRoute, ProtectedRoute } from '../util/route_utils';
 
-const App = () => (
-  <>
-    <header className="header">
-      <nav className="nav-bar">
-        <div className="left-nav">
-          <h1 className="title"><Link to="/">Pickle</Link></h1>
-          <Link className="nav-links" to="/"><i className="far fa-question-circle"></i> Search Sitters</Link>
-          <Link className="nav-links" to="/signup"><i className="far fa-heart"></i> Become a Sitter</Link>
-        </div>
-        <NavBarContainer />
-      </nav>
-    </header>
+const App = () => {
+  const headerClass = () => {
+    if (location.hash=== "#/" ) {
+      return "header home"
+    } else {
+      return "header"
+    }
+  };
 
-    <AuthRoute path="/signup" component={SignUpFormContainer} />
-    <AuthRoute path="/signin" component={SignInFormContainer} />
-  </>
-);
+  return(
+    <>
+      <header className={headerClass()}>
+        <nav className="nav-bar">
+          <div className="left-nav">
+            <h1 className="title"><Link to="/">Pickle</Link></h1>
+            <Link className="nav-links" to="/"><i className="far fa-question-circle"></i> Search Sitters</Link>
+            <Link className="nav-links" to="/signup"><i className="far fa-heart"></i> Become a Sitter</Link>
+          </div>
+          <NavBarContainer />
+        </nav>
+      </header>
+      
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+        <AuthRoute exact path="/signin" component={SignInFormContainer} />
+      </Switch>
+    </>
+  )
+};
 
 export default App;
---
+++ frontend/components/home/home.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <div className="home-container">
+        <div className="home-div">
+          <h1>Local, loving Guinea Pig Care</h1>
+          <h2>Book trusted sitters who'll treat your guinea pigs like family!</h2>
+          <div className="two-box-container">
+            <div className="form"></div>
+            <div className="other-form"></div>"
+          </div>
+        </div>
+      </div>
+      <div className="home-container-2">
+        <h3>Services for every guinea pig</h3>
+
+        <div className="two-box-container">
+          <div className="left">
+
+          </div>
+          <div className="right">
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
