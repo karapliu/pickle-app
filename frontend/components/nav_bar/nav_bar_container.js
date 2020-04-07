6259e2b0419b19598efed3d227dd82f671f35819
kara <karaliuu@gmail.com>
2020-04-07 10:13:02 -0400

    fixed bootstrapping problem!

--- frontend/components/nav_bar/nav_bar_container.js
+++ frontend/components/nav_bar/nav_bar_container.js
@@ -2,8 +2,8 @@ import { connect } from 'react-redux';
 import { logout } from '../../actions/session_actions';
 import NavBar from './nav_bar';
 
-const mapStateToProps = ({ session, entities: { members }}) => ({
-  currentMember: members[session.id]
+const mapStateToProps = state => ({
+  currentMember: state.session.currentMemberId
 });
 
 const mapDispatchToProps = dispatch => ({
