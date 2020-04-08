fe0418f1bef8152675e4c777dcb0f9481e28b642
kara <karaliuu@gmail.com>
2020-04-08 10:11:42 -0400

    re-rendering page to switch between home nav bar and other pages

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter, Route } from 'react-router-dom';
 import App from './App';
+import NavBarContainer from './nav_bar/nav_bar_container';
 
 const Root = ({ store }) => (
   <Provider store={store}>
